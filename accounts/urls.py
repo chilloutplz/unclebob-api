bf037b6d6cc03e651dc2a1586573c52e08c0837e
Uncle Bob <starnew@kakao.com>
2025-04-19 11:20:01 +0900

    login->token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import RegisterView, LoginView
 from rest_framework_simplejwt.views import(
-    # TokenObtainPairView,
+    TokenObtainPairView,
     # TokenVerifyView,
     # TokenBlacklistView,
     TokenRefreshView,
@@ -9,6 +9,6 @@ from rest_framework_simplejwt.views import(
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('token/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
