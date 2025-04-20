50af1bce13e7d50967def86c7e481bf2ec88542e
Uncle Bob <starnew@kakao.com>
2025-04-21 08:45:28 +0900

    bible view, url 수정정

--- bible/urls.py
+++ bible/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path('versions/', views.VersionListView.as_view(), name='version-list'),
-    path('<slug:version_slug>/books/', views.BookListView.as_view(), name='book-list'),
-    path('<slug:version_slug>/<slug:book_slug>/chapters/', views.ChapterListView.as_view(), name='chapter-list'),
-    path('<slug:version_slug>/<slug:book_slug>/<int:chapter>/verses/', views.VerseListView.as_view(), name='verse-list'),
+    path('<str:version_slug>/', views.BookListView.as_view(), name='book-list'),
+    path('<str:version_slug>/<str:book_slug>/', views.ChapterListView.as_view(), name='chapter-list'),
+    path('<str:version_slug>/<str:book_slug>/<int:chapter>/', views.VerseListView.as_view(), name='verse-list'),
+    path('<str:version_slug>/<str:book_slug>/<int:chapter>/<int:number>/', views.SingleVerseView.as_view(), name='single-verse'),
+    path('random/', views.RandomVerseView.as_view(), name='random-verse'),
     path('search/', views.VerseSearchView.as_view(), name='verse-search'),
 ]
--- bible/views.py
+++ bible/views.py
@@ -1,10 +1,11 @@
-# bible/views.py
-from django.db.models import Q
+import random
 
-from requests import Response
+from django.shortcuts import get_object_or_404
 
 from rest_framework import generics
-from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .models import Version, Book, Verse
 from .serializers import VersionSerializer, BookSerializer, VerseSerializer
@@ -18,7 +19,6 @@ class BookListView(generics.ListAPIView):
 
     def get_queryset(self):
         version_slug = self.kwargs['version_slug']
-        print('version_slug: ', version_slug)
         return Book.objects.filter(verses__version__slug=version_slug).distinct()
 
 class ChapterListView(generics.ListAPIView):
@@ -40,6 +40,37 @@ class VerseListView(generics.ListAPIView):
             chapter=chapter
         ).order_by('number')
 
+class SingleVerseView(RetrieveAPIView):
+    serializer_class = VerseSerializer
+
+    def get_object(self):
+        version_slug = self.kwargs['version_slug']
+        book_slug = self.kwargs['book_slug']
+        chapter = self.kwargs['chapter']
+        number = self.kwargs['number']
+        
+        return get_object_or_404(
+            Verse,
+            version__slug=version_slug,
+            book__slug=book_slug,
+            chapter=chapter,
+            number=number
+        )
+    
+class RandomVerseView(APIView):
+    def get(self, request):
+        count = Verse.objects.count()
+        if count == 0:
+            return Response({"error": "No verses found."}, status=404)
+        random_index = random.randint(0, count - 1)
+        verses = Verse.objects.all()[random_index]
+        return Response({
+            "version": verses.version.slug,
+            "book": verses.book.slug,
+            "chapter": verses.chapter,
+            "number": verses.number,
+            "text": verses.text
+        })
 class VerseSearchView(ListAPIView):
     serializer_class = VerseSerializer
 
