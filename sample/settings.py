cf655a2c4563f70933d120995e45a25562b22146
Uncle Bob <starnew@kakao.com>
2025-04-25 13:25:55 +0900

    REST_FRAMEWORK 인증관련 설정

--- sample/settings.py
+++ sample/settings.py
@@ -72,13 +72,16 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # RandomVerseView에서 오버라이드
+    ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
     ),
-    'UNICODE_JSON': False,
+    'UNICODE_JSON': True,
 }
 
 MIDDLEWARE = [
